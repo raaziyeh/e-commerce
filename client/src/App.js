887125fa2cb6997fd33a35f24dda476686e768d3
raaziyeh <rsoradimoghaddam@gmail.com>
2022-12-05 12:51:56 +0330

    Add the pages routes

---
+++ client/src/App.js
@@ -0,0 +1,44 @@
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom"
+import Home from "./pages/home/Home"
+import Products from "./pages/products/Products"
+import Product from "./pages/product/Product"
+import Navbar from "./components/navbar/Navbar"
+import Footer from "./components/footer/Footer"
+import { Children } from "react"
+
+const Layout = () => {
+	return (
+		<div className="app">
+			<Navbar />
+			<Outlet />
+			<Footer />
+		</div>
+	)
+}
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Layout />,
+		children: [
+			{
+				path: "/",
+				element: <Home />,
+			},
+			{
+				path: "/products/:id",
+				element: <Products />,
+			},
+			{
+				path: "/product/:id",
+				element: <Product />,
+			},
+		],
+	},
+])
+
+function App() {
+	return <RouterProvider router={router} />
+}
+
+export default App
