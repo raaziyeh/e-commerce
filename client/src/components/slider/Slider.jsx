6b10d536931dfee9a126a57ad061375652388b59
raziyeh <rsoradimoghaddam@gmail.com>
2023-05-03 11:43:36 +0430

    Fetched slider images from json-server

--- client/src/components/slider/Slider.jsx
+++ client/src/components/slider/Slider.jsx
@@ -1,16 +1,27 @@
 import EastIcon from "@mui/icons-material/East"
 import WestIcon from "@mui/icons-material/West"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import "./Slider.scss"
 
 const Slider = () => {
 	const [currentSlideIndex, setCurrentSlideIndex] = useState(0)
-	const images = [
-		"./img/slider_4.jpeg",
-		"./img/slider_1.jpeg",
-		"./img/slider_2.jpeg",
-		"./img/slider_3.jpeg",
-	]
+	const [images, setImages] = useState()
+
+	useEffect(() => {
+		async function getImages() {
+			try {
+				const response = await fetch("http://localhost:3004/slider")
+				if (response.ok) {
+					const data = await response.json()
+					setImages(data)
+				}
+			} catch(error) {
+				console.log("There is something wrong. please try again later")
+			}
+		}
+
+		getImages()
+	}, [])
 
 	const nextSlide = () => {
 		setCurrentSlideIndex((prev) => (prev === images.length - 1 ? 0 : prev + 1))
@@ -20,29 +31,35 @@ const Slider = () => {
 		setCurrentSlideIndex((prev) => (prev === 0 ? images.length - 1 : prev - 1))
 	}
 
+
+	if (!images || images.length === 0) {
+		return <p>Loading ... </p>
+	}
+
 	return (
-		<div className="slider">
-			<div
-				className="container"
-				style={{
-					width: `${100 * images.length}vw`,
-					transform: `translateX(-${currentSlideIndex * 100}vw)`,
-				}}
-			>
-				{images.map((img) => (
-					<img src={img} alt="model" key={img} />
-				))}
+	<div className="slider">
+		<div
+			className="container"
+			style={{
+				width: `${100 * images.length}vw`,
+				transform: `translateX(-${currentSlideIndex * 100}vw)`,
+			}}
+		>
+			{images.map((img) => (
+				<img src={img} alt="model" key={img} />
+			))}
+		</div>
+		<div className="arrows">
+			<div className="arrow-wrapper" onClick={prevSlide}>
+				<WestIcon className="arrow" />
 			</div>
-			<div className="arrows">
-				<div className="arrow-wrapper" onClick={prevSlide}>
-					<WestIcon className="arrow" />
-				</div>
-				<div className="arrow-wrapper" onClick={nextSlide}>
-					<EastIcon className="arrow" />
-				</div>
+			<div className="arrow-wrapper" onClick={nextSlide}>
+				<EastIcon className="arrow" />
 			</div>
 		</div>
+	</div>
 	)
+	
 }
 
 export default Slider
