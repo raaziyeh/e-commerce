5d89834a5a658fc0371fde96b54c291bde94bac9
raziyeh <rsoradimoghaddam@gmail.com>
2023-05-09 17:10:57 +0430

    Moved categories images files to remote uupload.ir

--- client/src/components/Categories/Categories.jsx
+++ client/src/components/Categories/Categories.jsx
@@ -1,53 +1,77 @@
+import { useState, useEffect } from "react"
 import { Link } from "react-router-dom"
 import "./Categories.scss"
 
 const Categories = () => {
-	return (
-		<div className="categories">
-			<div>
-				<img src="./img/trending_category.jpeg" alt="trending-category" />
-				<button>
-					<Link className="link">Trending</Link>
-				</button>
+	const [categoriesImages, setCategoriesImages] = useState()
+
+	useEffect(() => {
+		async function getCategoriesImages() {
+			try {
+				const response = await fetch(
+					"https://e-commerce-json-server.vercel.app/categories"
+				)
+
+				if (response.ok) {
+					const responseDate = await response.json()
+					setCategoriesImages(responseDate)
+				}
+			} catch (error) {
+				console.error(error)
+			}
+		}
+		getCategoriesImages()
+	}, [])
+
+	if (categoriesImages) {
+		const { kids, men, season, shoes, trending, women } = categoriesImages
+		return (
+			<div className="categories">
+				<div>
+					<img src={trending} alt="trending-category" />
+					<button>
+						<Link className="link">Trending</Link>
+					</button>
+				</div>
+				<div>
+					<img src={women} alt="women-category" />
+					<button>
+						<Link className="link" to="/products/women">
+							Women
+						</Link>
+					</button>
+				</div>
+				<div>
+					<img src={season} alt="season-category" />
+					<button>
+						<Link className="link">New Season</Link>
+					</button>
+				</div>
+				<div>
+					<img src={men} alt="men-category" />
+					<button>
+						<Link className="link" to="/products/men">
+							Men
+						</Link>
+					</button>
+				</div>
+				<div>
+					<img src={kids} alt="children-category" />
+					<button>
+						<Link className="link" to="/products/kids">
+							Children
+						</Link>
+					</button>
+				</div>
+				<div>
+					<img src={shoes} alt="shoes-category" />
+					<button>
+						<Link className="link">Shoes</Link>
+					</button>
+				</div>
 			</div>
-			<div>
-				<img src="./img/women_category.jpeg" alt="women-category" />
-				<button>
-					<Link className="link" to="/products/women">
-						Women
-					</Link>
-				</button>
-			</div>
-			<div>
-				<img src="./img/season_category.jpeg" alt="season-category" />
-				<button>
-					<Link className="link">New Season</Link>
-				</button>
-			</div>
-			<div>
-				<img src="./img/men_category.jpeg" alt="men-category" />
-				<button>
-					<Link className="link" to="/products/men">
-						Men
-					</Link>
-				</button>
-			</div>
-			<div>
-				<img src="./img/kids_category.jpeg" alt="children-category" />
-				<button>
-					<Link className="link" to="/products/kids">
-						Children
-					</Link>
-				</button>
-			</div>
-			<div>
-				<img src="./img/shoes_category.jpeg" alt="shoes-category" />
-				<button>
-					<Link className="link">Shoes</Link>
-				</button>
-			</div>
-		</div>
-	)
+		)
+	}
 }
 
 export default Categories
